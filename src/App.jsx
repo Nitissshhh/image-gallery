0570a40391b79a3b6ebc024511d815624f08aa63
Nitish Baral <128455004+Nitissshhh@users.noreply.github.com>
2025-09-27 14:29:02 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Gallery from './componets/Gallery'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>
+        <Gallery/>
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/Gallery.jsx
@@ -0,0 +1,51 @@
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css';
+import 'lightgallery/css/lg-fullscreen.css';
+import 'lightgallery/css/lg-share.css';
+import 'lightgallery/css/lg-rotate.css';
+
+// If you want you can use SCSS instead of css
+import 'lightgallery/scss/lightgallery.scss';
+import 'lightgallery/scss/lg-zoom.scss';
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+
+const images = Array.from({ length: 16 }, (_, i) => ({
+    src: `/${i + 1}.jpeg`,
+    alt: `${i + 1}`,
+}));
+
+export default function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
+            >
+              {images.map((image, index) => {
+                    return (
+                        <a href={image.src} key={index}>
+                            <img alt={image.alt} src={image.src} />
+                        </a>
+                    )
+                })}
+
+            </LightGallery>
+        </div>
+    );
+}
